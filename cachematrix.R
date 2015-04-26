41a8b2e9dca3f0c42905981d24ac53ba3bdb1d21
TRONOynx <kaitlyn.yeo@ca.pwc.com>
2015-04-26 11:08:10 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# The makeCacheMatrix function works like a class to create a list 
+# that contains the following sub-functions:
+# (a) set
+# (b) get
+# (c) setInv 
+# (d) getInv
 
 makeCacheMatrix <- function(x = matrix()) {
 
+# Define cache for inversion results
+  xinv <- NULL 
+  
+# Set a matrix to object created by the above pre-defined makeCacheMatrix function
+# And initialize xinv to null
+      set <- function(y) {
+	  x <<- y
+	  xinv <<- NULL 
+      }
+
+# Return the input matrix
+      get <- function() x 
+      
+# Set the inversed matrix
+      setInv <- function(inv) xinv <<- inv
+      
+# Return the inversed matrix
+      getInv <- function() xinv 
+
+# Create a list that contains the sub-functions (set > get > setInv > getInv) 
+# under the parent pre-defined function makeCacheMatrix
+      
+      list(set = set, get = get,
+	       setInv = setInv,
+	       getInv = getInv)
+
 }
 
-
-## Write a short comment describing this function
+# Get the inversed matrix from object x
+# Notes: 
+# (a) if uncalculated, it will be returned null value as defined above in xinv function
+# (b) if the inversion result is there, it will returned the calculated inversion
+# (c) if the inversion result is not there, it will get the matrix object using sub-function get and
+#     solve it using the function solve. Next, set it to the object using sub-function setInv before 
+#     returning the solved result
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getInv() 
+      if(!is.null(m)) {
+	  message("return cached data")
+	  return(m) 
+      }
+      data <- x$get() 
+      m <- solve(data) 
+      x$setInv(m) # 
+      m 
 }
